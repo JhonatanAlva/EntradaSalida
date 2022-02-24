80982405fe9a74c27c61b94534906eb37aadf14c
JhonatanAlvarado <jloola@miumg.edu.gt>
2022-02-23 22:12:27 -0600

    aster

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Fundamentos
+{
+    class EntradaSalida
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+
+            Console.WriteLine("Escribe tu nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre);
+            Console.Read();
+        }
+    }
+}
